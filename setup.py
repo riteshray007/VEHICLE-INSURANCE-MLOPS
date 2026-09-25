f4048be0c8e758a3509c6239a663adc21e780204
riteshray007 <ray.ritesh007@gmail.com>
2026-09-25 13:52:12 +0530

    folder structure, venv, dependencies set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ritesh Ray",
+    author_email="ray.ritesh007@gmail.com",
+    packages=find_packages()
+)
+
